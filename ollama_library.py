c5394936e27c2ed22770470c4033fc60f94e1b2e
sreejith1996 <pisharodysreejith96@gmail.com>
2025-03-14 09:54:58 -0400

    added streaming response for ollama library

--- ollama_library.py
+++ ollama_library.py
@@ -5,5 +5,7 @@ response = ollama.chat(model='llama3.2', messages=[
     'role': 'user',
     'content': 'Why is the sky blue?',
   },
-])
-print(response['message']['content'])
+], stream=True)
+
+for chunk in response:
+  print(chunk['message']['content'], end='', flush=True)
